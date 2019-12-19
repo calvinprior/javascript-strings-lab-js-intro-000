640559ccdccc194942ce512dc977c57da3659400
calvin <priorcalvin2@gmail.com>
2019-12-19 14:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "space";
-var space = "let's talk about space."
 
-var conversation =  ` let"s talk about + ${topic}`;
+var conversation =  `let"s talk about ${topic}.`;
